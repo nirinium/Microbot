96b4d7eecbd029bfc9c9f3c5c3a8c7bcb7b35f34
nirinium <wills.colton@gmail.com>
2026-01-19 16:41:32 -0500

    vork plugin edits

---
+++ runelite-client/src/main/java/net/runelite/client/plugins/microbot/PluginConstants.java
@@ -0,0 +1,40 @@
+package net.runelite.client.plugins.microbot;
+
+/**
+ * Shared constants for Microbot plugins.
+ * This class will be included in each plugin JAR during the shadow build process.
+ */
+public final class PluginConstants
+{
+
+    private PluginConstants()
+	  {
+        throw new UnsupportedOperationException("This is a utility class and cannot be instantiated");
+    }
+
+	public static final String DEFAULT_PREFIX = "<html>[<font color=#b8f704>MB</font>] ";
+	public static final String MOCROSOFT = "<html>[<font color=#b8f704M>M</font>] ";
+	public static final String BOLADO = "<html>[<font color=#FF0000><b>😡</b></font>] ";
+	public static final String GMASON = "<html>[<font color=#0077B6>G</font>] ";
+    public static final String SHOOTING_STAR_PREFIX = "<html>[<font color=#0077B6>✨</font>] ";
+	public static final String KSP = "<html>[<font color=#b8f704>KSP</font>] ";
+	public static final String STICKTOTHESCRIPT = "<html>[<font color=#FF4F00>STTS</font>] ";
+	public static final String MKE = "<html>[<font color=#48ecf7>MKE</font>] ";
+	public static final String CRANNY = "<html>[<font color=#00B4D8>🐬</font>] ";
+	public static final String Cardew = "<html>[<font color=#824BA3>CD</font>]";
+	public static final String BGA = "<html>[<font color=#FF6B35>bga</font>] ";
+    public static final String BASCHE = "<html>[<font color=#07A6F0>B</font>] ";
+    public static final String heapoverfl0w = "<html>[<font color=#07A6F0>h</font>] ";
+    public static final String Lumusi = "<html>[<font color=#FFD700><b>🤖</b></font>] ";
+    public static final String GIRDY = "<html>[<font color=#3DED97>ǥ</font>] ";
+    public static final String SEE1DUCK = "<html>[<font color=#ffff1a>🦆</font>] ";
+    public static final String GAGE = "<html>[<font color=#4287f5>Gage</font>] ";
+    public static final String ENGIN = "<html>[<font color=#BA8D8D>E</font>] ";
+    public static final String VIP = "<html>[<font color=#33AC6E>VIP</font>] ";
+    public static final String NATE = "<html>[<font color=orange>N</font>] ";
+    public static final String SYN = "<html>[<font color=orange>Syn</font>] ";
+    public static final String NIRI = "<html>[<font color=#FF5733>NIR</font>] ";
+
+    public static final boolean DEFAULT_ENABLED = false;
+    public static final boolean IS_EXTERNAL = true;
+}
---
+++ runelite-client/src/main/java/net/runelite/client/plugins/microbot/vorkath/State.java
@@ -0,0 +1,16 @@
+package net.runelite.client.plugins.microbot.vorkath;
+
+enum State {
+    BANKING,
+    TELEPORT_TO_RELLEKKA,
+    WALK_TO_VORKATH_ISLAND,
+    WALK_TO_VORKATH,
+    PREPARE_FIGHT,
+    FIGHT_VORKATH,
+    ZOMBIE_SPAWN,
+    ACID,
+    LOOT_ITEMS,
+    TELEPORT_AWAY,
+    DEAD_WALK,
+    SELLING_ITEMS
+}
